77cfe89b7e9b2d85731bc4fc22de0358eba7de56
zingkg <zingkg@msn.com>
2024-08-19 22:12:38 -0700

    Updated to version 0.3 for fixing use of deprecated preference manager

--- app/src/main/kotlin/com/zingkg/arkhamhorrorboard/secondedition/app/MainActivity.kt
+++ app/src/main/kotlin/com/zingkg/arkhamhorrorboard/secondedition/app/MainActivity.kt
@@ -3,7 +3,6 @@ package com.zingkg.arkhamhorrorboard.secondedition.app
 import android.content.Intent
 import android.content.res.Configuration
 import android.os.Bundle
-import android.preference.PreferenceManager
 import android.support.v4.app.Fragment
 import android.support.v4.app.FragmentManager
 import android.support.v4.app.FragmentStatePagerAdapter
@@ -12,6 +11,7 @@ import android.support.v4.view.ViewPager
 import android.support.v4.widget.DrawerLayout
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
+import android.support.v7.preference.PreferenceManager
 import android.support.v7.widget.Toolbar
 import android.view.Menu
 import android.view.MenuItem
@@ -19,7 +19,6 @@ import android.view.View
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.ListView
-import com.zingkg.arkhamhorrorboard.secondedition.app.R
 
 import java.io.InputStreamReader
 import java.io.Reader
